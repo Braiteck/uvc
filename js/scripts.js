116a4a5c2d2102e6f0889128ccd7df0ecb38a55f
Vitaliy Sulimenko <prevetal@gmail.com>
2020-10-24 10:03:31 +0300

    Переделана страница "Календарь"

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,108 @@ $(() => {
 			}
 		}
 	})
+
+
+	// Календарь - залипание блока
+	$('.your_order_fixed').scrollFix({
+		side: 'bottom'
+	})
+
+
+	// Календарь - покупка билета
+	$('body').on('click', '.calendar_buy .event .add label', function () {
+		let parent = $(this).closest('.add'),
+			available = parseInt($(this).find('.available span').text()),
+			inputVal = parseInt(parent.find('.amount .input').val())
+
+		if (inputVal > available) {
+			parent.find('.amount .input').addClass('error')
+			parent.find('.amount .maximum_error_text span').text(available)
+			parent.find('.amount .maximum_error_text').fadeIn(300)
+		} else {
+			parent.find('.amount .input').removeClass('error')
+			parent.find('.amount .maximum_error_text').fadeOut(200)
+		}
+	})
+
+	$('body').on('keydown', '.calendar_buy .event .add .amount .input', function () {
+		let parent = $(this).closest('.add'),
+			available = parseInt(parent.find('.type input:checked + label .available span').text()),
+			input = $(this)
+
+		setTimeout(() => {
+			let inputVal = parseInt(input.val())
+
+			if (inputVal > available) {
+				input.addClass('error')
+				parent.find('.amount .maximum_error_text span').text(available)
+				parent.find('.amount .maximum_error_text').fadeIn(300)
+			} else {
+				input.removeClass('error')
+				parent.find('.amount .maximum_error_text').fadeOut(200)
+			}
+		})
+	})
+
+
+	// Календарь - выбор даты
+	$('.calendar .days .day').click(function (e) {
+		e.preventDefault()
+
+		// открытие либо повторный клик
+		if (!$(this).hasClass('open') && $(this).find('.event.green').length) {
+			// Закрытие предыдущей открытой даты
+			$('.calendar .days .day').removeClass('open')
+
+			$('.calendar .days .calendar_buy').slideUp(200, () => {
+				$('.calendar .days .calendar_buy').remove()
+			})
+
+			// Открытие новой даты
+			setTimeout(() => {
+				// Получение данных (запрос на сервер)
+				$.ajax({
+					url: 'calendar_buy_template.html',
+					cache: false,
+					dataType: 'html',
+					type: "post",
+					success: (html) => {
+						// Успешное выполнение запроса
+						$(this).addClass('open')
+
+						// Высчитываем позицию
+						// Колво дней в строке
+						let daysInLine = parseInt($(this).css('--calendar_days_in_line'))
+
+						// Индекс нажатой даты
+						let currentIndex = parseInt($(this).index() + 1)
+
+						// Индекс даты после которой должен вставляться контент
+						let appendIndex
+
+						((currentIndex % daysInLine) == 0)
+							? appendIndex = currentIndex - 1
+							: appendIndex = currentIndex - 1 + (daysInLine - (currentIndex % daysInLine))
+
+						// Вставкаи показ html
+						$('.calendar .days > *').eq(appendIndex).after(html)
+						$('.calendar .days .calendar_buy').slideDown(300)
+
+						// Плавная прокрутка к новой дате
+						$('html, body').stop().animate({ scrollTop: $(this).offset().top - $('.mob_header').outerHeight() }, 500)
+
+						// Кастомный select
+						$('.calendar .days .calendar_buy select').niceSelect()
+					}
+				})
+			}, 300)
+		} else {
+			// Закрытие открытой даты
+			$('.calendar .days .day').removeClass('open')
+
+			$('.calendar .days .calendar_buy').slideUp(200, () => {
+				$('.calendar .days .calendar_buy').remove()
+			})
+		}
+	})
 })
